67ea8d6146ade903a28aec1c5783afe0c097ac78
Alex Martinez <zanderalex377@gmail.com>
2019-03-29 12:45:59 -0700

    add readme.md

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   $("button#submit").click(function(event){
     var answer = $("#answer").val();
-    var rightAnswer = $("#right-answer").val();
+    var Yes = $("#java").val();
 
     if (rightAnswer ==="1"){
       $(".answer2").show();
