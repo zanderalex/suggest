5c80fb07df0ceb829d5c016aff8bd34c63ce14cd
Alex Martinez <zanderalex377@gmail.com>
2019-03-29 15:56:51 -0700

    change form selector to radio buttons

---
+++ README.md
@@ -0,0 +1,31 @@
+_Course Selector_
+
+#### _This is a course suggesting tool for Epicodus.  answer the questions and get suggested courses as results.
+March, 2019_
+
+#### By _**Alex Martinez**_
+
+## Description
+
+_This is a course suggesting tool for Epicodus.  answer the questions and get suggested courses as results.  This tool utilizes knowledge obtained from the first two weeks at Epicodus._
+
+## Setup/Installation Requirements
+
+* _Clone this repo from https://github.com/zanderalex/suggest
+* _Launch the HTML file with a HTML browser_
+
+
+
+## Support and contact details
+
+_Alex Martinez: zanderalex377@gmail.com_
+
+## Technologies Used
+
+_HTML, CSS, Javascript, Jquery, Bootstrap_
+
+### License
+
+*This software is licensed under the GPL license.*
+
+Copyright (c) 2019 **_Alex Martinez_**
--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,47 @@
 $(document).ready(function(){
-  $("button#submit").click(function(event){
-    var answer = $("#answer").val();
-    var Yes = $("#java").val();
+  $("#userInput").submit(function(event){
 
-    if (rightAnswer ==="1"){
-      $(".answer2").show();
-    } else if (answer ==="2"){
-      alert('Please fill out questions.')
-    } else{
-      $(".answer1").show();
+    var answer = $("input:radio[name=answer]:checked").val();
+
+    if ("#javaanswer" === "Yes"){
+      $("#java").show();
+    } else if ("#javaanswer" === "No"){
+      $("#java").hide();
     }
+
+    var answer = $("input:radio[name=answer]:checked").val();
+
+    if ("#csharpanswer" === "Yes"){
+      $("#csharp").show();
+    } else if ("#csharpanswer" === "No"){
+      $("#csharp").hide();
+    }
+
+    var answer = $("#designanswer").val();
+
+    if ("#designanswer" === "Yes"){
+      $("#design").show();
+    } else if ("#dessignanswer" === "No"){
+      $("#design").hide();
+    }
+
+    var answer = $("#phpanswer").val();
+
+    if ("#phpanswer" === "Yes"){
+      $("#php").show();
+    } else if ("#phpanswer" === "No"){
+      $("#php").hide();
+    }
+
+    var answer = $("#rubyanswer").val();
+
+    if ("#rubyanswer" === "Yes"){
+      $("#ruby").show();
+    } else if ("#rubyanswer" === "No"){
+      $("#ruby").hide();
+    }
+
+
     event.preventDefault();
   });
 });
